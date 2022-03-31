5d353206cc99b5b04f80d1d7bd16dbbc27555d56
Lara <99837142+larathinkful@users.noreply.github.com>
2022-03-31 03:40:26 -0500

    Update accounts.js
    adding notes to help write functions

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,54 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+/*takes in two parameters, in the following order:
+- An array of account objects.
+- A string ID of a single account object.
+It returns the account object that has the matching ID.
+*/
+  
 
-function sortAccountsByLastName(accounts) {}
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function sortAccountsByLastName(accounts) {
+/*takes in a single parameter: an array of account objects.
+It returns a sorted array of the provided account objects. The objects are sorted alphabetically by last name.
+*/
 
-function getBooksPossessedByAccount(account, books, authors) {}
+  
+}
+
+function getTotalNumberOfBorrows(account, books) {
+/*takes in two parameters, in the following order:
+- An account object.
+- An array of all book objects.
+It returns a _number_ that represents the number of times the account's ID appears in any book's `borrows` array.
+*/
+  
+
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+/*takes in three parameters, in the following order:
+- An account object.
+- An array of all book objects.
+- An array of all author objects.
+It returns an array of book objects, including author information, that represents all books _currently checked out_ by the given account. 
+Look carefully at the object below, as it's not just the book object; the author object is nested inside of it.
+  {
+      id: "5f447132320b4bc16f950076",
+      title: "est voluptate nisi",
+      genre: "Classics",
+      authorId: 12,
+      author: {
+        id: 12,
+        name: {
+          first: "Chrystal",
+          last: "Lester",
+        },
+  */
+  
+  
+
+}
 
 module.exports = {
   findAccountById,
