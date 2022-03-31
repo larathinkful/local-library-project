5bd5ef31e7ab1ce5caacb95a33b70e2e85905565
Lara <99837142+larathinkful@users.noreply.github.com>
2022-03-31 03:47:44 -0500

    Update books.js
    added notes to help with function writing

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,44 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+ /*takes in two parameters, in the following order:
+  - An array of author objects.
+  - An integer ID of a single author object.
+  It returns the author object that has the matching ID.
+  */
+  
 
-function findBookById(books, id) {}
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function findBookById(books, id) {
+/*takes in two parameters, in the following order:
+- An array of book objects.
+- A string ID of a single book object.
+It returns the book object that has the matching ID.
+*/
 
-function getBorrowersForBook(book, accounts) {}
+
+}
+
+function partitionBooksByBorrowedStatus(books) {
+/*takes in a single parameter: an array of book objects
+It returns an array with two arrays inside of it. All of the inputted books are present in either the first or second array.
+The first array contains book objects that represent the books _that are currently checked out_, 
+while the second array contains book objects that represent the books _that have been returned_.
+You can check for the return status by looking at the first transaction object in the `borrows` array.
+*/
+  
+
+}
+
+function getBorrowersForBook(book, accounts) {
+/*takes in two parameters, in the following order:
+- A book object.
+- An array of all account objects.
+It should return an array of ten or fewer account objects that represents the accounts given by the IDs in the provided book's `borrows` array. 
+However, each account object should include the `returned` entry from the corresponding transaction object in the `borrows` array.
+*/
+  
+
+}
 
 module.exports = {
   findAuthorById,
