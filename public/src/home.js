0852e265cdf82d609bcc7ecebf4dc773a8cd88ba
Lara <99837142+larathinkful@users.noreply.github.com>
2022-03-31 11:46:38 -0500

    writing getTotalAccountsCount

--- public/src/home.js
+++ public/src/home.js
@@ -11,7 +11,8 @@ function getTotalAccountsCount(accounts) {
 /*takes in a single parameter: An array of accounts
 It returns a _number_ that represents the number of account objects inside of the array.
 */
-
+let total = 0;
+return accounts.reduce((accounts) => accounts + 1, total);
 
 }
 
