4d8a8c36fddb2f3b753007191116b4c269c34013
Lara <99837142+larathinkful@users.noreply.github.com>
2022-04-03 05:06:10 -0500

    writing getMostPopularAuthors

--- public/src/home.js
+++ public/src/home.js
@@ -77,8 +77,18 @@ Each object in the returned array has two keys:
 - The `count` key which represents the number of times the author's books have been borrowed.
 Even if there is a tie, the array should contain no more than five objects.
 */
-
-
+let popAuthors = [];
+  authors.forEach((author) => {
+    const theAuthor = {  name: `${author.name.first} ${author.name.last}`,
+   count: 0 };
+    books.forEach((book) => { 
+      if (book.authorID === author.id) {
+        author.count += book.borrows.length;
+      }
+    });
+    popAuthors.push(theAuthor);
+  });
+return popAuthors.sort((a, b) => b.count - a.count).slice(0, 5);
 }
 
 module.exports = {
